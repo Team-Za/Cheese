a70c85a79feecd41bc188f5a19d4e9d3b16953f8
Hayden R <raydenhoberts@gmail.com>
2018-04-13 10:29:19 -0700

    Added api page and homepage

---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = props => (
+ <div className="nav-bar">
+    <div className="logo">Stock Up</div>
+</div>
+);
+
+export default Navbar;
---
+++ client/src/Components/Symbols.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Symbols = props => (
+  <div className="text-center">
+    <p>{props.result}</p>
+  </div>
+);
+
+export default Symbols;
