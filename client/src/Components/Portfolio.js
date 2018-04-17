2cfa21c53b82e49c0f17d9ec112344e252d735ee
Joseph Tomoyasu <JTomoyasu@gmail.com>
2018-04-17 11:22:13 -0700

    seeds

--- client/src/Components/Portfolio.js
+++ client/src/Components/Portfolio.js
@@ -3,30 +3,39 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { portApi, stockApi, userApi } from "../utils/serverAPI";
 import '../Views/app.scss';
 import Navbar from "./Navbar";
+import Portview from "./Portview";
 
 class Portfolio extends React.Component {
     state = {
         result: []
     };
-
     async componentDidMount() {
         await this.searchPortfolios(1);
     }
-
     //   searchSymbols = query => {
     //     API.allSymbols(query)
     //       .then(res => { console.log(res.data); this.setState({ result: res.data }) })
     //       .catch(err => console.log(err));
     //   };
-    searchPortfolios = id => {
-        portApi.getPortfolioAndStocksbyUserId(id)
-            .then(res => { console.log(res); this.setState({ result: res }) })
+    searchPortfolios = async id => {
+        await portApi.getPortfolioAndStocksbyUserId(id)
+            .then(res => { console.log(res,new Date()); this.setState({result:res})})
             .catch(err => console.log(err));
     };
+    // setPortfolio = async id =>{
+    //     let stocks = [];
+    //     if(stocks = await this.searchPortfolios(id)){
+    //         await this.setState({result:stocks});
+    //     }
+    // }
     render() {
         return (
             <div className="container">
-                {console.log(this.state.result)}
+                {console.log(this.state.result,new Date())}
+                <Portview
+                    userName={this.state.result.userName}
+                    Stocks={this.state.result.Stocks}
+                />
             </div>
         );
     }
