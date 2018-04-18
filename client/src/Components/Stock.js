d886e986f30a8621ae3ec4e19e8f71cf590a457d
Joseph Tomoyasu <JTomoyasu@gmail.com>
2018-04-17 23:27:14 -0700

    Posting Stocks Alpha version 1.0

--- client/src/Components/Stock.js
+++ client/src/Components/Stock.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Stock = props => (
     <div className="stock">
         <div>
-            Stock: {props.name}
-            Symbol: {props.symbol}
-            Image: {props.imageLink}
+            Stock: {props.name} 
+            Symbol: {props.symbol} 
+            Image: <img src={props.imageLink}/>
             Quantity: {props.quantity}
             Price: {props.price}
         </div>
