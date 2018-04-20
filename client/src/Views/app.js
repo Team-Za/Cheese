e3bf4de7a12c7cbd9e597a049b6df0bd83d6bfc9
David Turner <Dturner753@yahoo.com>
2018-04-20 09:53:00 -0700

    Auto stash before merge of "Development" and "origin/Development"
    merging development and changes

--- client/src/Views/app.js
+++ client/src/Views/app.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import Organization from './Organization';
-import HomeContainer from "../Components/HomeContainer";
-import Home from "../Components/Home";
 import LandingPage from "../Components/LandingPage";
-import API from "../utils/API";
 import './app.scss';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes.js';
