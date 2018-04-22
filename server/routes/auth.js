bcd0cf5ab10e5c5441f9a4975f30181039bd9705
David Turner <Dturner753@yahoo.com>
2018-04-22 13:01:18 -0700

    Auto stash before merge of "david" and "Development"

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -96,14 +96,14 @@ router.post('/signup', (req, res, next) => {
 
   return passport.authenticate('local-signup', (err) => {
     if (err) {
-      if (err.name === 'MySQL Error' && err.code === 11000) {
+      if (err.username === 'MySQL Error' && err.username == "DuplicateUsername") {
         // the 11000 Mongo code is for a duplication email error
         // the 409 HTTP status code is for conflict error
         return res.status(409).json({
           success: false,
           message: 'Check the form for errors.',
           errors: {
-            email: 'This email is already taken.'
+            username: 'This username is already taken.'
           }
         });
       }
