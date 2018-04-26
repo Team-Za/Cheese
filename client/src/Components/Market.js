af23ac2e5b70cb680a577409cf7e538f86d95e4e
Hayden R <raydenhoberts@gmail.com>
2018-04-26 16:25:26 -0700

    Got rid of value in input box so it shows placeholder

--- client/src/Components/Market.js
+++ client/src/Components/Market.js
@@ -467,7 +467,7 @@ class Market extends React.Component {
                   <div className="field-line">
                     <input
                       className="sign-up-inputs"
-                      value={this.state.quantity}
+                      
                       onChange={this.handleInputChange}
                       name="quantity"
                       placeholder="Quantity (required)"
